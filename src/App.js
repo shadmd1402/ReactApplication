706d47709aa41f4b1a4863085524043ce695db88
shadmd1402 <43815460+shadmd1402@users.noreply.github.com>
2024-02-01 13:42:52 +0530

    Test for delete functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react'
-import { BrowserRouter, Routes , Route } from 'react-router-dom'
-import VideosList from './pages/VideosList'
-import Hello from './demo/Hello'
-import Dashboard from './pages/Dashboard'
-import Products from './product/Products'
-import AddProduct from './product/AddProduct'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import VideosList from "./pages/VideosList";
+import Hello from "./demo/Hello";
+import Dashboard from "./pages/Dashboard";
+import Products from "./product/Products";
+import AddProduct from "./product/AddProduct";
 
 function App() {
-    const userdetails = {username: 'Shad', description:'desasxas asxasx asxasxa ', phone:''};
+  const userdetails = {
+    username: "Shad",
+    description: "desasxas asxasx asxasxa ",
+    phone: "",
+  };
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<VideosList />} />
-            <Route path='/hello' element={<Hello />} />
-            <Route path='/dashboard' element= {<Dashboard user= {userdetails} />} />
-            <Route path='/products' element={<Products />} />
-            <Route path='/addproduct' element={<AddProduct />} />
-          {/* /asxajbckas */}
-        </Routes>
+      <Routes>
+        <Route path="/" element={<VideosList />} />
+        <Route path="/hello" element={<Hello />} />
+        <Route path="/dashboard" element={<Dashboard user={userdetails} />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/addproduct" element={<AddProduct />} />
+        {/* /asxajbckas */}
+        {/* sdhjcbskcdbsk */}
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
